0cd5a503000821e6e94464a2ef80bb949c64f264
Sausageexpert <68068569+Sausageexpert@users.noreply.github.com>
2021-09-08 21:04:31 +0530

    Add files via upload

---
+++ swapStuff.py
@@ -0,0 +1,21 @@
+file1 = input("Enter File To Modify (And Hopefully Not Eat) ")
+file2 = input("Enter Another File (Which May Or May Not Be Eaten) ")
+    
+def swap():
+    with open(file1, 'r') as info1:
+        realInfo1 = info1.read()
+    
+        
+    with open(file2, 'r') as info3:
+        realInfo3 = info3.read()
+        
+    with open(file1, 'w') as info4:
+        info4.write(realInfo3)
+    with open(file2, 'w') as info2:
+        info2.write(realInfo1)
+        
+swap()
+
+
+# Please work? Please?
+# I'll keep bugging you until you work, you know...
